1f45eae59f50763b64d635f57546861932f49e45
Oscar Ahumada Gómez <116230587+OscarAhumadaG@users.noreply.github.com>
2024-03-21 22:03:49 -0400

    Update utils.py

--- ResNetClassifierStreamlit/utils.py
+++ ResNetClassifierStreamlit/utils.py
@@ -2,16 +2,9 @@ import numpy as np
 
 
 
-# returns the top1 string
-def print_prob(prob, file_path, top_n=1):
-    synset = [l.strip() for l in open(file_path).readlines()]
-
-    # Get probabilities
-    prob_np = prob.numpy()
-    pred = np.argsort(prob_np)[::-1]
-
+def print_prob(prob, class_names, top_n=1):
     # Get top N labels
-    top_n_labels = [(synset[pred[i]], prob_np[pred[i]]) for i in range(top_n)]
+    top_n_labels = [(class_names[i], prob[i]) for i in range(top_n)]
     return top_n_labels
 
 
