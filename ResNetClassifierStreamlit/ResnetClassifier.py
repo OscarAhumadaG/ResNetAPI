9b620e6382a502180dc01d98a0d02f20597e31d9
Oscar Ahumada Gómez <116230587+OscarAhumadaG@users.noreply.github.com>
2024-03-21 22:04:41 -0400

    Update ResnetClassifier.py

--- ResNetClassifierStreamlit/ResnetClassifier.py
+++ ResNetClassifierStreamlit/ResnetClassifier.py
@@ -49,7 +49,7 @@ if btn_classify and  uploaded_file is not None:
         with torch.no_grad():
             output = torch.nn.functional.softmax(resnet50(input_batch), dim=1)
     
-        results = utils.print_prob(output[0], class_names)
+        results = utils.print_prob(output[0], class_names, top_n=1)
         
         """st.title("Image Results")
         
